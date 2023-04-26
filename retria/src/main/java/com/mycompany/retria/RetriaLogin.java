05255eb5c805756de4175655d46231171ad9e4fd
LucasSilvvaLLima <lucas.lima@sptech.school>
2023-04-26 18:53:54 -0300

    Update - PING da SPTECH

--- retria/src/main/java/com/mycompany/retria/RetriaLogin.java
+++ retria/src/main/java/com/mycompany/retria/RetriaLogin.java
@@ -209,9 +209,9 @@ public class RetriaLogin extends javax.swing.JFrame {
                 @Override
                 public void run() {
                     try {
-                        ping.execCommand("ping 44.204.81.68");
+                        ping.execCommand("ping 10.18.34.148");
                     } catch (IOException ex) {
-                        System.out.println(new IOException(String.format("Falha ao executar comando %s. Erro: %s", "ping 44.204.81.68", ex.toString())));
+                        System.out.println(new IOException(String.format("Falha ao executar comando %s. Erro: %s", "ping 10.18.34.148", ex.toString())));
                     }
                 }
             }, 2000, 2000);
