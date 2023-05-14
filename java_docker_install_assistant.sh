dce4eefb7c4f4a6b47186882de9a38b46ef45155
kaiosenaa <kaio.sena@sptech.school>
2023-05-14 16:37:19 -0300

    Modificando código de instalacao docker.

--- java_docker_install_assistant.sh
+++ java_docker_install_assistant.sh
@@ -31,7 +31,7 @@ if [ $? -eq 0 ]
 			if [ $VERSAO -eq 17 ]
 				then
 					echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 17 do Java. Confirme a instalação quando solicitado ;D"
-					sudo apt install default-jre ; apt install openjdk-17-jre-headless; -y
+					sudo apt install default-jre ; sudo apt install openjdk-17-jre-headless; -y
 					clear
 					echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
 			fi
