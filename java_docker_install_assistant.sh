dd49dd89bd6ce1251f889701c6af87e0404b0697
kaiosenaa <kaio.sena@sptech.school>
2023-05-29 23:23:34 -0300

    Alteração na condição de criação do container/dockerfile

--- java_docker_install_assistant.sh
+++ java_docker_install_assistant.sh
@@ -13,8 +13,6 @@ java --version
 if [ $? -eq 0 ]; then
     echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java 17 já está instalado."
 else
-
-
     echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o repositório do Java 17"
     sleep 2
 
@@ -24,7 +22,6 @@ else
     echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalando o Java 17"
     sleep 2
     apt-get install openjdk-17-jdk -y
-
 
     java --version
     if [ $? -eq 0 ]; then
@@ -36,6 +33,7 @@ fi
 sleep 5
 
 echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando a aplicação da Retria."
+
 
 wget github.com/GP3-2-ADSC/umms-back-end-java-cli/raw/main/retria/target/retria-1.0-SNAPSHOT-jar-with-dependencies.jar
 
@@ -66,47 +64,46 @@ read inst
 if [ "$inst" != "S" ] && [ "$inst" != "s" ]; then
     echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalação cancelada. Até a próxima!"
     exit 0
-fi
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando as dependências para adicionar o repositório via HTTPS"
-sudo  apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-sleep 2
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o par de chaves do Docker"
-
-##  curl faz requisições HTTP
-##  gpg(dearmor) usada para manipular chaves, ela está salvando a chave no seguinte caminho -> /usr/share/keyrings/docker-archive-keyring.gpg
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sleep 2
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o repositório do Docker..."
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalando o Docker..."
-
-apt install -y docker.io
-
-sleep 5
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Iniciando serviço e configurando docker"
-
-systemctl enable docker
-sudo service docker start
-sleep 2
-
-echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Verificando versão do docker"
-docker --version
-if [ $? -eq 0 ]; then
-    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Docker foi instalado com sucesso."
 else
-    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Houve um erro durante a instalação do Docker."
-fi
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando as dependências para adicionar o repositório via HTTPS"
+    sudo  apt install -y apt-transport-https ca-certificates curl software-properties-common
 
+    sleep 2
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o par de chaves do Docker"
+
+    ##  curl faz requisições HTTP
+    ##  gpg(dearmor) usada para manipular chaves, ela está salvando a chave no seguinte caminho -> /usr/share/keyrings/docker-archive-keyring.gpg
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sleep 2
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o repositório do Docker..."
+
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt update
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalando o Docker..."
+
+    apt install -y docker.io
+
+    sleep 5
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Iniciando serviço e configurando docker"
+
+    systemctl enable docker
+    sudo service docker start
+    sleep 2
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Verificando versão do docker"
+    docker --version
+    if [ $? -eq 0 ]; then
+        echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Docker foi instalado com sucesso."
+        else
+        echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Houve um erro durante a instalação do Docker."
+    fi
+fi
 echo  "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Você deseja efetuar a criação de um container? (S/N)"
 read inst
 
